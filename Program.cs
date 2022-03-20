2d97549390cb49d1c5a03f53708d612bd83a3133
ibebb <ibebb@DESKTOP-EQNPII5>
2022-03-20 18:32:12 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Pv;
+
+using (PvRecorder recorder = PvRecorder.Create(0, frameLength: 1024))
+{
+    Console.WriteLine($"Using device: {recorder.SelectedDevice}");
+    Console.Write($"Listening...\n");
+    recorder.Start();
+
+    while (true)
+    {
+        short[] pcm = recorder.Read();
+
+        Console.WriteLine($"Read {pcm.Length} bytes");
+
+        Thread.Yield();
+    }
+}
